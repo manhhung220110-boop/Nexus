9295b7f0602e0865bb73401c8c1ea7e18c3b9f78
manhhung220110-boop <manhhung220110@gmail.com>
2026-09-21 19:20:03 +0700

    Update router.tsx

--- nexus-foundation/src/router.tsx
+++ nexus-foundation/src/router.tsx
@@ -1,24 +1,50 @@
-import { createBrowserRouter } from 'react-router-dom'
-import { LandingPage } from './pages/LandingPage'
-import { SignInPage } from './pages/SignInPage'
-import { SignUpPage } from './pages/SignUpPage'
-import { OAuthCallbackPage } from './pages/OAuthCallbackPage'
-import { HomePage } from './pages/HomePage'
-import { AppShell } from './components/layout/AppShell'
-import { AuthGuard } from './components/auth/AuthGuard'
+import { createBrowserRouter } from 'react-router-dom';
+import { LandingPage } from './pages/LandingPage';
+import { SignInPage } from './pages/SignInPage';
+import { SignUpPage } from './pages/SignUpPage';
+import { OAuthCallbackPage } from './pages/OAuthCallbackPage';
+import { HomePage } from './pages/HomePage';
+import { ComingSoonPage } from './pages/ComingSoonPage';
+import { AppShell } from './components/layout/AppShell';
+import { AuthGuard } from './components/auth/AuthGuard';
 
-export const router = createBrowserRouter([
-  { path: '/', element: <LandingPage /> },
-  { path: '/auth/signin', element: <SignInPage /> },
-  { path: '/auth/signup', element: <SignUpPage /> },
-  { path: '/auth/callback', element: <OAuthCallbackPage /> },
-  {
-    path: '/home',
+function protectedRoute(path: string, element: React.ReactNode) {
+  return {
+    path,
     element: (
       <AuthGuard>
         <AppShell />
       </AuthGuard>
     ),
-    children: [{ index: true, element: <HomePage /> }],
+    children: [
+      {
+        index: true,
+        element: element,
+      },
+    ],
+  };
+}
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingPage />,
   },
-])
+  {
+    path: '/auth/signin',
+    element: <SignInPage />,
+  },
+  {
+    path: '/auth/signup',
+    element: <SignUpPage />,
+  },
+  {
+    path: '/auth/callback',
+    element: <OAuthCallbackPage />,
+  },
+  protectedRoute('/home', <HomePage Heck />),
+  protectedRoute('/messages', <ComingSoonPage title="Tin nhắn" />),
+  protectedRoute('/music', <ComingSoonPage title="Âm nhạc" />),
+  protectedRoute('/watch', <ComingSoonPage title="Video" />),
+  protectedRoute('/settings', <ComingSoonPage title="Cài đặt" />),
+]);
